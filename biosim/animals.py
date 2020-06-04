711836888b5e2d34b35145f185cee4ff716263a8
mikalbre <mibreite@nmbu.no>
2020-06-04 15:05:25 +0200

    Removed some methods

--- biosim/animals.py
+++ biosim/animals.py
@@ -7,6 +7,7 @@ __email__ = 'astrised@nmbu.no, mibreite@nmbu.no'
 
 from math import exp
 
+
 class Animals:
     """
     Animal parent class, i.e. all animals in the simulation must be subclasses of this parent class.
@@ -35,7 +36,6 @@ class Animals:
             else:
                 raise ValueError("Parameter not defined for this animal")  # DeltaPhiMax for carni
 
-
     def __init__(self, age, weight):
         """
 
@@ -56,9 +56,8 @@ class Animals:
         self.has_migrated = False
         self.compute_fitness = True
 
-        self.fitness = 0
+        self.phi = 0
         self.fitness_calculation()
-
 
     def fitness_calculation(self):
         """
@@ -66,82 +65,17 @@ class Animals:
         Fitness depends on weight and age of animal.
         :return:
         """
-        positive_q = (1/(1+exp(self.params["phi"]*(self.age() - self.params["a_half"]))))
-        negative_q = (1/(1+exp(-self.params["phi"]*(self.weight - self.params["w_half"]))))
+        positive_q = (1/(1+exp(self.phi*(self.age - self.params["a_half"]))))
+        negative_q = (1/(1+exp(-self.phi*(self.weight - self.params["w_half"]))))
 
         if self.weight == 0:
-            self.params["phi"] = 0
+            self.phi = 0
         else:
-            self.params["phi"] = positive_q*negative_q
-        return self.params["phi"]
-
-    def fitness(self):
-        """
-        Calculate fitness if weight or age to animal is changed.
-
-        :return:
-        """
-        if self.compute_fitness == True:
-            if self.weight <= 0:
-                return 0
-
-        self.compute_fitness = False
-        self.fitness = fitness_calculation()
-
+            self.phi = positive_q*negative_q
+        return self.phi
 
     def age(self):
         """ Adds an increment of 1 to age, i.e. age increases by 1 each year."""
         self.age += 1
         self.fitness_calculation()
-
-
-    def birth(self):
-        """
-        Whether an animal will give birth.
-        If animal gives birth, weight decreases and return offspring.
-        :return:
-        """
-        pass
-
-
-    def weight(self):
-        """ """
-        pass
-
-    def decrease_weight(self):
-        """
-        Weight of animal decreases passively each year.
-        Annual weight loss
-
-        :return:
-        """
-        pass
-
-
-    def feed(self):
-        """
-        Animal eats food in cell.
-        Weight of animal gets update.
-        Returns new amount of fodder left in cell
-
-        :return:
-        """
-        pass
-
-    def can_migrate(self):
-        """
-        If animal has not moved to new cell, it calculates whether it will move or not.
-
-        :return:
-        """
-        pass
-    def death(self):
-        """
-        Whether an animal dies is calculated by probability.
-        Fitness = 0, animal will die.
-
-        :return:
-        """
-        pass
-
 
