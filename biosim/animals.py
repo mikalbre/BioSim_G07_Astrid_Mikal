ac79a32127231b3334c4c28fa20ac563d2ee7f70
mikalbre <mibreite@nmbu.no>
2020-06-04 11:38:20 +0200

    Started making the parameters

--- biosim/animals.py
+++ biosim/animals.py
@@ -8,70 +8,13 @@ class Animals:
     It represents a single animal, and does not specify the type of animal.
     It contains methods, variables and properties that are common for both carnivore and herbivore.
     """
-    """
-        Sets the class parameters and check if input is in the right format.
-
-        :param w_birth: float
-            average birth weight
-        :param sigma_birth: float
-            std of birth weight
-        :param beta: float
-            beta multiplied by fodder increases animal weight
-        :param eta: float
-            eta multiplied by weight, decreases animal weight each year
-        :param a_half: float
-            Age component of fitness calculation, scalar
-        :param phi_age: float
-            Age component of fitness calculation, scalar
-        :param w_half: float
-            Weight component of fitness calculation, scalar
-        :param phi_weight: float
-            Weight component of fitness calculation, scalar
-        :param mu: float
-            scalar for moving, animal moves with probability mu is multiplied with fitness
-            Constant for the probability of migrating
-        :param gamma: float
-            scalar for birth, probability for birth
-            Probability of birth constant
-        :param zeta: float
-            scalar if birth will happen
-            Probability constant for birth relative to weight
-        :param xi: float
-            Scalar for decrease in mother weight, decreases motherweight by zeta*birthweight
-        :param omega: float
-            scalar for death, probability is omega*(1-fitness)
-            Death probability factor, scalar
-        :param F: float
-            Animals appetite, amount of food required for an animal in a year
-            Maximum food capacity
-        :param DeltaPhiMax: float
-            Used by carnivore to calculate if they can kill a herbivore.
-            Maximum difference in fitness between carnivore and herbivore.
-
-        :return:
-    """
-    w_birth = 8.0
-    sigma_birth = 1.5
-    beta = 0.9
-    eta = 0.05
-    a_half = 40
-    phi_age = 0.6
-    w_half = 10
-    phi_weight = 0.1
-    mu = 0.25
-    gamma = 0.2
-    zeta = 3.5
-    xi = 1.2
-    omega = 0.4
-    F = 10.0
-
-    default_parameters = {'w_birth': None, 'sigma_birth': None, 'beta': None, 'eta': None, 'a_half': None,
-                          'phi_age': None, 'w_half': None, 'phi_weight': None, 'mu': None, 'gamma': None, 'zeta': None,
-                          'xi': None, 'omega': None, 'F': None, 'DeltaPhiMax': None}
+    params = None
 
     @classmethod
-    def set_parameters(cls, new_parameters):
-        pass
+    def set_parameters(cls,params):
+        animal_set_parameters = params.update()
+
+
 
     def __init__(self):
         """"""
@@ -138,7 +81,84 @@ class Animals:
 
 
 class herbivores(Animals):
-    pass
+    default_parameters = {'w_birth': 8.0,
+                          'sigma_birth': 1.5,
+                          'beta': 0.9,
+                          'eta': 0.05,
+                          'a_half': 40,
+                          'phi_age': 0.6,
+                          'w_half': 10,
+                          'phi_weight': 0.1,
+                          'mu': 0.25,
+                          'gamma': 0.2,
+                          'zeta': 3.5,
+                          'xi': 1.2,
+                          'omega': 0.4,
+                          'F': 10,
+                          'DeltaPhiMax': None}
+
+
+
 
 class carnivores(Animals):
     pass
+
+
+
+   """
+        Sets the class parameters and check if input is in the right format.
+
+        :param w_birth: float
+            average birth weight
+        :param sigma_birth: float
+            std of birth weight
+        :param beta: float
+            beta multiplied by fodder increases animal weight
+        :param eta: float
+            eta multiplied by weight, decreases animal weight each year
+        :param a_half: float
+            Age component of fitness calculation, scalar
+        :param phi_age: float
+            Age component of fitness calculation, scalar
+        :param w_half: float
+            Weight component of fitness calculation, scalar
+        :param phi_weight: float
+            Weight component of fitness calculation, scalar
+        :param mu: float
+            scalar for moving, animal moves with probability mu is multiplied with fitness
+            Constant for the probability of migrating
+        :param gamma: float
+            scalar for birth, probability for birth
+            Probability of birth constant
+        :param zeta: float
+            scalar if birth will happen
+            Probability constant for birth relative to weight
+        :param xi: float
+            Scalar for decrease in mother weight, decreases motherweight by zeta*birthweight
+        :param omega: float
+            scalar for death, probability is omega*(1-fitness)
+            Death probability factor, scalar
+        :param F: float
+            Animals appetite, amount of food required for an animal in a year
+            Maximum food capacity
+        :param DeltaPhiMax: float
+            Used by carnivore to calculate if they can kill a herbivore.
+            Maximum difference in fitness between carnivore and herbivore.
+
+        :return:
+    """
+
+w_birth = 8.0
+sigma_birth = 1.5
+beta = 0.9
+eta = 0.05
+a_half = 40
+phi_age = 0.6
+w_half = 10
+phi_weight = 0.1
+mu = 0.25
+gamma = 0.2
+zeta = 3.5
+xi = 1.2
+omega = 0.4
+F = 10.0
