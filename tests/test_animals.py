0dc461e56f842cdd709717cfb3fb13d700c08aed
Astrid <astrised@nmbu.no>
2020-06-04 15:06:00 +0200

    Added test for set_parameters

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,1 +1,20 @@
 """Tests the animals.py file in the biosim folder."""
+
+from biosim import animals
+import pytest
+
+
+@pytest.fixture
+def set_parameters(request):
+    animals.set_parameters(request.params)
+    yield
+    animals.set_parameters(animals.params)
+
+
+
+def test_init():
+    
+    herb = Herbivores(5, 40)
+
+    assert herb.age == 5
+    assert herb.weight == 40
