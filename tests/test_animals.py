49feff700dba3f469e37dc0f85152c9b0065118d
Astrid <astrised@nmbu.no>
2020-06-04 21:34:42 +0200

    Added several test

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,6 +1,6 @@
 """Tests the animals.py file in the biosim folder."""
 
-from biosim.animals import Animals
+from biosim.animals import Animals, Herbivore
 import pytest
 
 
@@ -10,24 +10,42 @@ def set_parameters(request):
     yield
     Animals.set_parameters(Animals.params)
 
-def test_age():
-    a = Animals()
-    for n in range(10):
-        a.age_increase()
-        assert a.age_increase() == n + 1
-
-def test_fitness_calulation():
-    a = Animals()
-
-
-
-
 
 def test_init():
-    
     herb = Herbivore(5, 40)
 
     assert herb.age == 5
     assert herb.weight == 40
 
-#def test_Herbivore_issubclass():
+
+def test_annual_age_increase():
+    herbivore = Herbivore(3, 12)
+    assert herbivore.age == 3
+
+    herbivore.annual_age_increase()
+    assert herbivore.age == 4
+
+
+def test_fitness_calulation():
+    herb = Herbivore(6, 0)
+    assert herb.phi == 0
+
+    herb = Herbivore(2, 13)
+    assert not herb.phi == 0
+
+
+def test_annual_weight_decrease():
+    herb = Herbivore(5, 40)
+    herb.annual_weight_decrease()
+
+    assert herb.weight < 40
+
+
+def test_procreation():
+    herb = Herbivore(4, 30)
+    
+
+
+
+
+
