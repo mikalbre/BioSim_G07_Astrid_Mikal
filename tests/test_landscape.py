b8fa784415172e634ffe745739ea1df52896c036
mikalbre <mibreite@nmbu.no>
2020-06-10 20:27:00 +0200

    test_landscape

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -9,7 +9,7 @@ def test_init():
     pass
 
 def test_age():
-    herb = Herbivore(8,3)
+    herb = Herbivore(8, 3)
     assert herb.get_age() == 8
 
 def test_lowland_instance():
@@ -37,6 +37,100 @@ def test_animal_allocates():
     lowland = Lowland()
     lowland.animals_allocate(ini_animal)
     assert len(lowland.present_herbivores) == 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TestHighland:
+    def test_init(self):
+        highland = Highland()
+        assert type(highland.present_herbivores) is list
+        assert type(highland.present_carnivores) is list
+        assert highland.get_fodder() == 300
+
+
+class TestLowland:
+    def test_init(self):
+        lowland = Lowland()
+        assert type(lowland.present_herbivores) is list
+        assert type(lowland.present_carnivores) is list
+        assert lowland.get_fodder() == 800
+
+class TestDesert:
+    def test_init(self):
+        desert = Desert()
+        assert type(desert.present_herbivores) is list
+        assert type(desert.present_carnivores) is list
+        assert desert.get_fodder() == 0
+
+class TestWater:
+    def test_init(self):
+        water = Water()
+        assert type(water.present_herbivores) is list
+        assert type(water.present_carnivores) is list
+        assert water.get_fodder() == 0
+
+
+
+def test_procreation(): # Blir 50 < 50
+    lowland = Lowland()
+    pop_herb = [{'species': 'Herbivore', 'age': 5, 'weight': 20} for _ in range(50)]
+    len_pop_herb = len(pop_herb)
+    lowland.animals_allocate(pop_herb)
+    lowland.procreation()
+    len_popHerb = len(pop_herb)
+    assert len_pop_herb < len_popHerb
+
+def test_procreation():
+    lowland = Lowland()
+    pop_herb = [{'species': 'Herbivore', 'age': 5, 'weight': 20} for _ in range(50)]
+    len_pop_herb = len(pop_herb)
+    lowland.animals_allocate(pop_herb)
+    lowland.procreation()
+    len_popHerb = len(pop_herb)
+    assert len_pop_herb < len_popHerb
+
 
 
 
