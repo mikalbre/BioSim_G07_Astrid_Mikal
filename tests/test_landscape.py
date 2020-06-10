639395e4fc8f387df22d3855b98d7bd5d783c307
Astrid <astrised@nmbu.no>
2020-06-10 15:20:46 +0200

    Added testfile for landscape.py

---
+++ tests/test_landscape.py
@@ -0,0 +1,11 @@
+from biosim.animals import Herbivore, Carnivore
+from biosim.landscape import Lowland, Highland, Desert, Water
+
+def test_cell_parameters():
+    pass
+
+def test_init():
+    pass
+
+def test_animals_allocates():
+    pass
